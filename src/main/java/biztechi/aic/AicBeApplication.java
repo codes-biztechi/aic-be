e15cf74fd0ea75bd6c6f52b52d0e1ddb7735d8d9
codesver <codesver@gmail.com>
2024-03-26 21:10:30 +0900

    init

---
+++ src/main/java/biztechi/aic/AicBeApplication.java
@@ -0,0 +1,12 @@
+package biztechi.aic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AicBeApplication.class, args);
+    }
+}
---
+++ src/test/java/biztechi/aic/AicBeApplicationTests.java
@@ -0,0 +1,13 @@
+package biztechi.aic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
